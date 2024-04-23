4b09c0c052832dfde28fbe2beba6066dae5b11ec
Giordan Zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-04-23 18:49:58 +0000

    html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Giordan Zeina All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Giordan Zeina
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+console.log(randomNumber)
+
+function enterClicked() {
+  // input
+  const numberGuessed = parseInt(document.getElementById("guess").value)
+
+  // output
+  if (numberGuessed == randomNumber) {
+    document.getElementById("answer").innerHTML = 'You guessed the correct number! The Number was ' + randomNumber + '!'
+  }
+
+  if (numberGuessed != randomNumber) {
+    document.getElementById("answer").innerHTML = 'You guessed the wrong number, Try again!'
+  }
+}
